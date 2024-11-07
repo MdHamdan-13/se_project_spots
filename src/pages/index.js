bce1e030dc1b5d6feacdca2b89bcc41d4ef60475
Mohamed Hamdan <hamdan41d@gmail.com>
2024-11-07 16:56:58 -0600

    Api Update and added avatar modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,21 +57,29 @@ const api = new Api({
 
 api
   .getAppInfo()
-  .then(([cards]) => {
+  .then(([cards, userInfo]) => {
     // console.log(cards);
+    // console.log(userInfo);
     cards.forEach((cardItem) => {
       const cardElement = getCardElement(cardItem);
       cardsList.prepend(cardElement);
     });
+
+    // handle userInfo
+    //set the src of the avatar image
+    //set the textContent of both text elements;
   })
   .catch((err) => {
     console.error(err);
   });
 
+// Profile Elements
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const profileAddButton = document.querySelector(".profile__add-btn");
+const avatarModalButton = document.querySelector(".profile__avatar-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
+const profileAvatar = document.querySelector(".profile__avatar");
 
 const modalEdit = document.querySelector("#edit-modal");
 const modalEditForm = modalEdit.querySelector(".modal__form");
@@ -81,18 +89,28 @@ const modalDescriptionInput = modalEdit.querySelector(
   "#profile-description-input"
 );
 
+// Card Elements
 const modalCard = document.querySelector("#add-card-modal");
 const modalCardForm = modalCard.querySelector(".modal__form");
 const cardSubmitBtn = modalCard.querySelector(".modal__submit-btn");
 const profileCardCloseBtn = modalCard.querySelector(".modal__close-btn");
 const cardLinkInput = modalCard.querySelector("#add-card-link-input");
 const cardCaptionInput = modalCard.querySelector("#add-card-caption-input");
+
+// Preview Elements
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImg = document.querySelector(".modal__image");
 const previewModalText = document.querySelector(".modal__caption");
 const previewModalClose = previewModal.querySelector(
   ".modal__close-btn_type_preview"
 );
+
+// Avatar Elements
+const avatarModal = document.querySelector("#avatar-modal");
+const avatarForm = avatarModal.querySelector(".modal__form");
+const avatarSubmitBtn = avatarModal.querySelector(".modal__submit-btn");
+const avatarCloseBtn = avatarModal.querySelector(".modal__close-btn");
+const avatarLinkInput = avatarModal.querySelector("#profile-avatar-input");
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
@@ -157,9 +175,31 @@ function closeModal(modal) {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = modalNameInput.value;
-  profileDescription.textContent = modalDescriptionInput.value;
-  closeModal(modalEdit);
+  api
+    .editUserInfo({
+      name: modalNameInput.value,
+      about: modalDescriptionInput.value,
+    })
+    .then((data) => {
+      profileName.textContent = data.name;
+      profileDescription.textContent = data.about;
+      closeModal(modalEdit);
+    })
+    .catch(console.error);
+}
+
+function handleAvatarSubmit(evt) {
+  evt.preventDefault();
+
+  api
+    .editAvatarInfo({
+      avatar: avatarLinkInput.value,
+    })
+    .then((data) => {
+      // console.log(data);
+      profileAvatar.src = data.avatar;
+    })
+    .catch(console.error);
 }
 
 function handleCardFormSubmit(evt) {
@@ -202,8 +242,17 @@ previewModalClose.addEventListener("click", () => {
   closeModal(previewModal);
 });
 
+avatarModalButton.addEventListener("click", () => {
+  openModal(avatarModal);
+});
+
+avatarCloseBtn.addEventListener("click", () => {
+  closeModal(avatarModal);
+});
+
 modalEditForm.addEventListener("submit", handleProfileFormSubmit);
 modalCardForm.addEventListener("submit", handleCardFormSubmit);
+avatarForm.addEventListener("submit", handleAvatarSubmit);
 
 // initialCards.forEach((cardItem) => {
 //   const cardElement = getCardElement(cardItem);
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -8,11 +8,10 @@ class Api {
   }
 
   getAppInfo() {
-    return Promise.all([this.getInitialCards()]);
+    return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
 
   getInitialCards() {
-    // ...
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
     }).then((res) => {
@@ -23,7 +22,50 @@ class Api {
     });
   }
 
-  // other methods for working with the API
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  editUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  editAvatarInfo({ avatar }) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+
+      body: JSON.stringify({
+        avatar,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: ${res.status}`);
+    });
+
+    // other methods for working with the API
+  }
 }
 
 // export the class
